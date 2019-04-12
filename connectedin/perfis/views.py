ca35680840c93ec5671405a7fd77a3859e0e9a02
gusilva <gusilva@padtec.com.br>
2019-04-11 21:47:59 -0300

    2147

--- connectedin/perfis/views.py
+++ connectedin/perfis/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 '''
 Ao digitar http://localhost:8000/ ele chama essa pagina e retorna o index.html
@@ -6,9 +7,15 @@ Ao digitar http://localhost:8000/ ele chama essa pagina e retorna o index.html
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {'perfis': Perfil.objects.all()})
 
 
 def exibir(request, perfil_id):
-    print 'ID do perfil recebido -> %s ' % (perfil_id)
-    return render(request, 'perfil.html')
+    # print 'ID do perfil recebido -> %s ' % (perfil_id)
+    # perfil = Perfil()
+    perfil = Perfil.objects.get(id=perfil_id)
+
+    # if perfil_id == '1':
+    # perfil = Perfil('Gustavo', 'gu@terra.com', '3838-5638', 'Padtec')
+
+    return render(request, 'perfil.html', {"perfil": perfil})
